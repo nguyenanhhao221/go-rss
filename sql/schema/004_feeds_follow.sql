0f768f16fcec636500c1ace23b32d8eec9aada67
Hao Nguyen <hao@haonguyen.tech>
2023-05-27 16:27:24 +0700

    fix(sql): user_id and feed_id must be unique in feed_follow

--- sql/schema/004_feeds_follow.sql
+++ sql/schema/004_feeds_follow.sql
@@ -4,7 +4,8 @@ CREATE TABLE feeds_follow (
     create_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    feed_id UUID NOT NULL REFERENCES feeds(id) ON DELETE CASCADE
+    feed_id UUID NOT NULL REFERENCES feeds(id) ON DELETE CASCADE,
+    UNIQUE(user_id, feed_id)
 );
 -- +goose Down
 DROP TABLE feeds_follow;
