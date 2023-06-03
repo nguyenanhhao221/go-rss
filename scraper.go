2fb30e4658bf01bc67efc7b63d82db1a254c7e6d
Hao Nguyen <hao@haonguyen.tech>
2023-06-04 02:50:55 +0700

    feat: save fetch post to database

--- scraper.go
+++ scraper.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
 	"sync"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/nguyenanhhao221/go-rss/internal/database"
 )
 
@@ -59,7 +61,33 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 	}
 
 	for _, item := range rssFeed.Channel.Item {
-		log.Printf("Found post %v on feed %v", item.Title, feed.Name)
+
+		// Convert type
+		description := sql.NullString{}
+		if item.Description != "" {
+			description.String = item.Description
+			description.Valid = true
+		}
+
+		// Parse date
+		pubDate, err := time.Parse(time.RFC1123Z, item.PubDate)
+		if err != nil {
+			log.Printf("Could not parse date %v with err %v", item.PubDate, err)
+		}
+
+		_, createPostErr := db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreateAt:    time.Now().UTC(),
+			UpdatedAt:   time.Now().UTC(),
+			Title:       item.Title,
+			Description: description,
+			PublishedAt: pubDate,
+			Url:         item.Link,
+			FeedID:      feed.ID,
+		})
+		if createPostErr != nil {
+			log.Printf("Could not create post %v", createPostErr)
+		}
 	}
 	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
 }
